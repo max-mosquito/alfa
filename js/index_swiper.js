eee620d9fbdb0fd56ca6b7a1034d51f4efd61f13
MaxMosquito <w-2aa@mail.ru>
2023-10-13 15:41:09 +0300

    fix слайдеры на главной

--- js/index_swiper.js
+++ js/index_swiper.js
@@ -54,20 +54,20 @@ const swiper_main = new Swiper('.slider_main', {
 
   breakpoints: {
     320: {
-      slidesPerView: 1.2,
+      slidesPerView: 1,
       spaceBetween: 20
     },
     575: {
-      slidesPerView: 1.5,
+      slidesPerView: 2,
       spaceBetween: 40
     },
     991: {
-      slidesPerView: 2.5,
+      slidesPerView: 3,
       spaceBetween: 40
     },
-    1400: {
-      slidesPerView: 3.5,
-      spaceBetween: 40
-    },
+    // 1400: {
+    //   slidesPerView: 3.5,
+    //   spaceBetween: 40
+    // },
   }
 });
