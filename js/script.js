4344e1aa42a3d0453b9407b52815c71b5041216d
MaxMosquito <w-2aa@mail.ru>
2023-10-14 15:14:41 +0300

    fix header dropdown menu

--- js/script.js
+++ js/script.js
@@ -1,35 +1,58 @@
+//burger-----------------
 const burger = document.getElementById('burger');
 const headerTopMenu = document.getElementById('header_top_menu');
+const closeTopMenu = document.getElementById('close_top_menu');
 
 burger.addEventListener('click', () => {
-   burger.classList.toggle('active');
-   headerTopMenu.classList.toggle('active');
-   document.body.classList.toggle('lock');
+  //  burger.classList.add('active');
+   headerTopMenu.classList.add('active');
+   document.body.classList.add('lock');
+})
+
+closeTopMenu.addEventListener('click', () => {
+  headerTopMenu.classList.remove('active');
+  document.body.classList.remove('lock');
 })
 
 //catalog-----------------------------
-const catalogBtn = document.getElementById('catalog');
-const dropdownCatalog = document.getElementById('dropdown_header');
-const listTechBtn = document.querySelectorAll('.dropdown_header_lis_item');
-const techPic= document.querySelectorAll('.dropdown_header_pic_item');
-
-catalogBtn.addEventListener('click', () => {
-  catalogBtn.classList.toggle('active');
-  dropdownCatalog.classList.toggle('active');
-  document.body.classList.toggle('lock');
-})
+const catalogBtn = document.querySelectorAll('.catalogBtn');
+const dropdownPic = document.querySelectorAll('.dropdown_header_pic_item');
 
 function hidePic() {
-  techPic.forEach(item => {
+  dropdownPic.forEach(item => {
     item.classList.remove('active')
   })
 }
 
-listTechBtn.forEach((item, i) => {
+catalogBtn.forEach((item, i) => {
   item.addEventListener('mouseover', () => {
-    hidePic();
-    techPic[i - 1].classList.add('active');
+    hidePic()
+    dropdownPic[i].classList.add('active');
   })
 })
+
+// const catalogBtn = document.getElementById('catalog');
+// const dropdownCatalog = document.getElementById('dropdown_header');
+// const listTechBtn = document.querySelectorAll('.dropdown_header_lis_item');
+// const techPic= document.querySelectorAll('.dropdown_header_pic_item');
+
+// catalogBtn.addEventListener('click', () => {
+//   catalogBtn.classList.toggle('active');
+//   dropdownCatalog.classList.toggle('active');
+//   document.body.classList.toggle('lock');
+// })
+
+// function hidePic() {
+//   techPic.forEach(item => {
+//     item.classList.remove('active')
+//   })
+// }
+
+// listTechBtn.forEach((item, i) => {
+//   item.addEventListener('mouseover', () => {
+//     hidePic();
+//     techPic[i - 1].classList.add('active');
+//   })
+// })
 
 
