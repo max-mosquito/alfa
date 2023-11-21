e2cca42b40b77c833ca3e2bde9416d96ba1e985b
Max_Mosquito <w-2aa@mail.ru>
2023-11-21 09:52:07 +0300

    add slider on Service

---
+++ js/service_slider.js
@@ -0,0 +1,10 @@
+const service = new Swiper('.service_slider', {
+   // Default parameters
+   slidesPerView: 1,
+   spaceBetween: 10,
+   // autoplay:true,
+   pagination: {
+      el: '.service_pagination',
+    },
+ })
+ 
